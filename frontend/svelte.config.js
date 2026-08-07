65085b7b3039c69aee3c739d2157042d699cdd93
Jos3C190 <josecarloslopezm17@gmail.com>
2026-08-07 15:10:45 -0600

    feat: configure SvelteKit project and add initial backend Dockerfile

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
